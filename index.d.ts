17bc58d5b4907cde26ea39125642cfce56cf5a96
Albert Portnoy <albert@albertp.dev>
2021-10-24 13:15:31 -0500

    Fix params

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,41 @@
 declare module 'mongoose-fuzzy-searching' {
-  import { Document, DocumentQuery, Model, Schema } from 'mongoose';
+	import {Document, Query, FilterQuery, Model, Schema} from 'mongoose';
 
-  export interface MongooseFuzzyOptions<T> {
-    fields: (T extends Object ? keyof T : string)[];
-  }
+	export interface MongooseFuzzyOptions<T> {
+		fields: (T extends Object ? keyof T : string)[];
+	}
 
-  export interface MongooseFuzzyModel<T extends Document, QueryHelpers = {}>
-    extends Model<T, QueryHelpers> {
-    fuzzySearch(
-      search: String,
-      callBack?: (err: any, data: Model<T, QueryHelpers>[]) => void,
-    ): DocumentQuery<T[], T, QueryHelpers>;
-  }
+	export type Search =
+		| string
+		| {
+				query: string;
+				minSize?: number;
+				prefixOnly?: boolean;
+				exactOnly?: boolean;
+		  };
 
-  function fuzzyPlugin<T>(schema: Schema<T>, options: MongooseFuzzyOptions<T>): void;
+	type Callback<T, QueryHelpers> = (
+		err: any,
+		data: Model<T, QueryHelpers>[],
+	) => void;
 
-  export default fuzzyPlugin;
+	export interface MongooseFuzzyModel<T extends Document, QueryHelpers = {}>
+		extends Model<T, QueryHelpers> {
+		fuzzySearch(
+			query: Search,
+			additionalQuery?: FilterQuery<T>,
+			callback?: Callback<T, QueryHelpers>,
+		): Query<T[], T, QueryHelpers>;
+		fuzzySearch(
+			query: Search,
+			callback?: Callback<T, QueryHelpers>,
+		): Query<T[], T, QueryHelpers>;
+	}
+
+	function fuzzyPlugin<T>(
+		schema: Schema<T>,
+		options: MongooseFuzzyOptions<T>,
+	): void;
+
+	export default fuzzyPlugin;
 }
